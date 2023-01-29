94a0c12f2f374be0da5177ecec9c4cb315550224
AbdulHadi <abdulhadi91478@gmail.com>
2023-01-30 00:54:57 +0500

    do it

---
+++ index.js
@@ -0,0 +1,12 @@
+ 
+  let x = document.querySelector("h2").innerHTML = 0;
+ function add(){
+    //  document.querySelector("h2").innerHTML;
+    x.innerHTML = x++;
+    
+ }
+
+ function subtract(){
+    //  document.querySelector("h2").innerHTML;
+    x.innerHTML = x--;
+ }
